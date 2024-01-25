a6f134614e985adf96ba0c985937059080cdf42b
chins.git <chientd.work@gmail.com>
2024-01-25 08:21:54 +0700

    change

--- vCenter-auto/Esxi-Config-syslog-server.ps1
+++ vCenter-auto/Esxi-Config-syslog-server.ps1
@@ -6,7 +6,7 @@ foreach ($ESXI_HOST in $ESXI_HOSTS) {
     Write-Host "Updating global syslog settings for ESXi host $($ESXI_HOST.Name)..."
 
     # Change global syslog host with UDP protocol
-    Set-VMHostAdvancedConfiguration -VMHost $ESXI_HOST -Name "Syslog.global.logHost" -Value "udp://192.168.99.250:514" -Confirm:$false
+    Set-VMHostAdvancedConfiguration -VMHost $ESXI_HOST -Name "Syslog.global.logHost" -Value "udp://192.168.1.1:514" -Confirm:$false
 }
 
 # Disconnect from vCenter server
